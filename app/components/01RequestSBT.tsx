67876268019a986ab2b1f1ab69a1bd5793df82e3
Miquel A. Cabot <miquel.cabot@gmail.com>
2023-09-29 19:35:37 +0200

    add social login

--- app/components/01RequestSBT.tsx
+++ app/components/01RequestSBT.tsx
@@ -3,23 +3,64 @@
 import { useState } from 'react';
 import { Wallet, ethers } from "ethers";
 import { Button, Form, Message } from 'semantic-ui-react';
+import { CHAIN_NAMESPACES, SafeEventEmitterProvider } from "@web3auth/base";
+import { Web3Auth } from "@web3auth/modal";
 
 export default function RequestSBT (props: any) {
+  const [idToken, setIdToken] = useState('');
   const [privateKey, setPrivateKey] = useState('');
   const [publicKey, setPublicKey] = useState('');
   const [address, setAddress] = useState('');
 
-  const generatePrivateKey = () => {
-    const pKey = ethers.Wallet.createRandom().privateKey;
+  const login = async () => {
+    const web3auth = new Web3Auth({
+      clientId: process.env.WEB3_AUTH_CLIENT_ID || "BB9-HFtHLnNBeMZhxvALkBrMqwJjuSZNTiE2gd9mwnUzrmqLGKXER07oE3WTcZkjlE4ZKw6lxEoE-Rx6QfoihI4",
+      web3AuthNetwork: "sapphire_mainnet",
+      chainConfig: {
+        chainNamespace: CHAIN_NAMESPACES.EIP155,
+        chainId: "0x5",
+        rpcTarget: "https://rpc.ankr.com/eth_goerli",
+        displayName: "Goerli",
+        blockExplorer: "https://goerli.etherscan.io",
+        ticker: "ETH",
+        tickerName: "Ethereum"
+      },
+    });
+
+    await web3auth.initModal();
+
+    if (!web3auth) {
+      throw new Error("web3auth not initialized yet");
+    }
+    const web3authProvider = await web3auth.connect();
+    if (!web3authProvider) {
+      throw new Error("web3authprovider not initialized yet");
+    }
+
+    console.log(web3auth);
+
+    if (!web3auth.provider) {
+      throw new Error("web3authprovider not initialized yet");
+    }
+    const authenticateUser = await web3auth.authenticateUser();
+
+    const pKey = await getPrivateKey(web3auth.provider);
+    setIdToken(authenticateUser.idToken);
+    setPrivateKey("0x"+pKey);
+
     const wallet: Wallet = new ethers.Wallet(
-      pKey,
+      "0x"+pKey,
       ethers.getDefaultProvider("goerli")
       );
-      
-    setPrivateKey(pKey);
     setPublicKey(wallet.publicKey);
     setAddress(wallet.address);
   }
+
+  const getPrivateKey = async (provider: SafeEventEmitterProvider) => {
+    return (await provider.request({
+      method: "private_key",
+    })) as string;
+  };
 
   const requestSBT = () => {
     props.setPrivateKey(privateKey);
@@ -33,11 +74,12 @@ export default function RequestSBT (props: any) {
         attached
         icon='user'
         color='green'
-        header='Generate private key & request SBT'
+        header='Login, get private key & request SBT'
         content='Identity holder'
       />
       <Form className='attached fluid segment'>
-        <Button color='blue' onClick={generatePrivateKey}>Generate Private Key</Button>
+        <Button color='blue' onClick={login}>Login</Button>
+        <Form.Input label='Id token' type='text' value={idToken} readOnly error/>
         <Form.Input label='Private key' type='text' value={privateKey} readOnly error/>
         <Form.Input label='Public key' type='text' value={publicKey} readOnly error/>
         <Form.Input label='Address' type='text' value={address} readOnly error/>
