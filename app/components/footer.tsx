b2c45c1d9e941a7ee63795acff7c226836539a88
Miquel A. Cabot <miquel.cabot@gmail.com>
2023-10-04 09:16:32 +0200

    remove name from copyright

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -2,7 +2,7 @@ export default function Footer() {
   return (
     <div className="ui vertical footer segment form-page">
       <div className="ui container">
-        Miquel A. Cabot &copy; 2023
+        &copy; 2023
       </div>
     </div>
   )
